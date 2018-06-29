3e83e97b492aaaee107b6e9d03a900b04263ed30
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2018-06-29 12:59:49 +0200

    collection summary custom implementation

--- app/printTemplates/summary/ca_collections_summary.php
+++ app/printTemplates/summary/ca_collections_summary.php
@@ -26,7 +26,7 @@
  * -=-=-=-=-=- CUT HERE -=-=-=-=-=-
  * Template configuration:
  *
- * @name Object summary
+ * @name Collection summary
  * @type page
  * @pageSize letter
  * @pageOrientation portrait
@@ -49,21 +49,47 @@
 	print $this->render("pdfStart.php");
 	print $this->render("header.php");
 	print $this->render("footer.php");	
-?>
-	<br/>
+?><br>
 	<div class="title">
 		<?php print $t_item->getLabelForDisplay();?>
 	</div>
 
 <?php
-	foreach($va_placements as $vn_placement_id => $va_bundle_info){
-		if (!is_array($va_bundle_info)) break;
-		if (!strlen($vs_display_value = $t_display->getDisplayValue($t_item, $vn_placement_id, array('purify' => true)))) {
-			if (!(bool)$t_display->getSetting('show_empty_values')) { continue; }
-			$vs_display_value = "&lt;"._t('not defined')."&gt;";
-		}
+    print '<div class="data"><span class="label">idno'." </span><span class='meta'> {$t_item->getWithTemplate('^ca_collections.idno')}</span></div>\n";
+    print '<div class="data"><span class="label">naam'." </span><span class='meta'> {$t_item->getWithTemplate('^ca_collections.hierarchy.preferred_labels.name%delimiter=__➔__')}</span></div>\n";
+    $objects = $t_item->getRelatedItems('ca_objects');
+    $t_object = new ca_objects();
 
-		print '<div class="data" style="margin-top: 20px; padding-bottom: 10px"><span class="label">'."{$va_bundle_info['display']} </span><span class='meta'> {$vs_display_value}</span></div>\n";
-	}
+    print '<div class="data"><span class="label">gerelateerde objecten'." </span></div>";
+    foreach ($objects as $item => $object ){
+        $t_object->load($object['object_id']);
+        $datering = $t_object->get('ca_objects.objectProductionDate');
+        $width = $t_object->get('ca_objects.dimensions.dimensions_width');
+        $height = $t_object->get('ca_objects.dimensions.dimensions_height');
+        $depth = $t_object->get('ca_objects.dimensions.dimensions_depth');
 
+        $image_reps = array();
+        //$image_reps = $t_object->getRepresentations(array("thumbnail"), null, array('return_primary_only' => true));
+        if(!empty($image_reps) && is_array($image_reps))
+            $image = current($image_reps);
+
+        print '<div style="page-break-inside: avoid;">';
+
+            print '<div style="page-break-inside: avoid; float:left; width: 30%; height: 60px; margin:10px; padding-top: 5px">';
+                if(!empty($image))
+                    print '<img src="data:image/jpeg;base64,'.base64_encode(file_get_contents($image['paths']['thumbnail'])).' width="70" height="70"">'."\n";
+            print '</div>';
+
+            print '<div style="font-size: 12px; page-break-inside: avoid; padding-top: 5px; float:left; margin:10px;width: 50%">';
+                print "{$object['idno']}<br>{$object['name']}<br>Datering: {$datering}<br>";
+                if(!empty($width))
+                    print "{$width} B x ";
+                if(!empty($height))
+                    print "{$height} H x ";
+                if(!empty($depth))
+                    print "{$depth} D";
+            print '</div>';
+
+        print '</div>';
+    }
 	print $this->render("pdfEnd.php");
